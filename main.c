bd0d73f362128a5c3ee168bd661725834814bbdb
AstridIsVeryCool <146063339+AstridIsVeryCool@users.noreply.github.com>
2024-02-19 01:37:11 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 typedef struct
 {
@@ -25,11 +27,73 @@ void set_element(matrix *m, int row, int col, double element)
 
 double get_element(matrix *m, int row, int col)
 {
+    if(0 > row && 0 > col && col >= m->columns && row >= m->rows)
+    {
+        return m->data[row*col];
+    }
+}
 
-    return m->data[row*col];
+void free_matrix(matrix *m)
+{
+    free(m->data);
+}
+
+void display_matrix(matrix *m)
+{
+    printf("_");
+    for(int i = (m->columns) * (length_of_longest_int_in_matrix(&m)+2); i >= 0; i--)
+    {   
+        printf(" ");
+    }
+    printf("_\n");
+    int current_row;
+    for(current_row = 0; current_row < m->rows; current_row++)
+    {
+        for (int current_column = 0; current_column < m->columns; current_column++)
+        {
+
+        }
+    }
+}
+
+int get_length_of_int(int x)
+{
+    return (int)(ceil(log10(x)));
+}
+
+int length_of_longest_int_in_matrix(matrix *m)
+{
+    int longest_length = 0;
+    for(int i = 0; i < m-> columns * m->rows; i++)
+    {
+        if(get_length_of_int(m->data[i]) > longest_length)
+        {
+            longest_length = get_length_of_int(m->data[i]);
+        }
+    }
+    return longest_length;
 }
 
 int main()
 {
-
+    printf("Welcome to Astrid's Epic and Based Manual Matrix Manipulation!!!!!!");
+    int running = 1;
+    int valid_choice = 0;
+    int choice = 0;
+    matrix test_matrix;
+    init_matrix(&test_matrix, 2, 2);
+    set_element(&test_matrix, 1,1, 4.0);
+    while (valid_choice == 0)
+    {
+        printf("please select from one of the following choices:\n");
+        printf("1: Display matrix\n");
+        scanf("%d", &choice);
+        if(choice == 1)
+        {
+            display_matrix(&test_matrix);
+        }
+        
+    }
+    
+    return 0;
 }
